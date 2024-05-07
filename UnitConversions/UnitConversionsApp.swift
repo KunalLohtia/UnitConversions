f53a414ba25f3f72454546559ca535883531d01a
kunallohtia <klohtia@ucdavis.edu>
2024-05-07 16:56:34 -0700

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Kunal L on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
