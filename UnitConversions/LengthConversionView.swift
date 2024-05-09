c88a8cbc71cf70925fecd23e07f04226e1757bfa
kunallohtia <klohtia@ucdavis.edu>
2024-05-08 17:20:41 -0700

    made home screen, temp conversions, and length conversions screens

---
+++ UnitConversions/LengthConversionView.swift
@@ -0,0 +1,18 @@
+//
+//  LengthConversionView.swift
+//  UnitConversions
+//
+//  Created by Kunal L on 5/7/24.
+//
+
+import SwiftUI
+
+struct LengthConversionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LengthConversionView()
+}
---
+++ UnitConversions/VolumeConversionView.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeConversionView.swift
+//  UnitConversions
+//
+//  Created by Kunal L on 5/7/24.
+//
+
+import SwiftUI
+
+struct VolumeConversionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VolumeConversionView()
+}
